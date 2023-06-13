f36c70b0f01a2568bb41caa6feea0d5510342f30
stephane <stephane@khedhiri.com>
2023-06-13 13:19:34 +0200

    add UserApiErrors

--- packages/apiCoach/src/lambda/get.ts
+++ packages/apiCoach/src/lambda/get.ts
@@ -1,16 +1,16 @@
-import {getById} from '@mycoach/core/repositories/coach.repositories'
-import {usePathParams} from "sst/node/api";
 
-export const handler = async () => {
+import {usePathParams} from "sst/node/api";
+import {APIGatewayProxyHandlerV2} from "aws-lambda";
+export const handler: APIGatewayProxyHandlerV2 = async (events, context, callback) => {
 
         // get params request
         const {id} = usePathParams()
         if(!id){
             throw new Error('param id not found');
         }
-        const coach = await getById(id)
+
         return {
             statusCode:200,
-            body: coach,
+            body: JSON.stringify(coach),
         }
 }
