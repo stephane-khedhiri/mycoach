20f9290766e2e6506c983d40d1b1cd621d726b59
stephane <stephane@khedhiri.com>
2023-06-13 14:33:54 +0200

    fix lambda get

--- packages/apiCoach/src/lambda/get.ts
+++ packages/apiCoach/src/lambda/get.ts
@@ -1,16 +1,4 @@
-import {getById} from '@mycoach/core/repositories/coach.repositories'
-import {usePathParams} from "sst/node/api";
 
 export const handler = async () => {
 
-        // get params request
-        const {id} = usePathParams()
-        if(!id){
-            throw new Error('param id not found');
-        }
-        const coach = await getById(id)
-        return {
-            statusCode:200,
-            body: coach,
-        }
 }
