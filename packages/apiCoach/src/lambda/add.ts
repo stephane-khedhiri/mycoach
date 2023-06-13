2222ba4823b97122f6c872a9d7997d3156a6cd23
stephane <stephane@khedhiri.com>
2023-06-13 15:16:24 +0200

    add validator

--- packages/apiCoach/src/lambda/add.ts
+++ packages/apiCoach/src/lambda/add.ts
@@ -2,20 +2,26 @@ import {IRegisterCoach, RegisterCoachDto} from "../dto/coach.dto";
 import {validate, registerSchema, ValidationError} from "class-validator";
 import {CoachRepository} from "../repositories/coach.repositories";
 import {APIGatewayProxyEventV2} from "aws-lambda";
+import {UserError} from "@mycoach/core/util/errors";
 export const handler = async (event: APIGatewayProxyEventV2 ) => {
 
         const datas = event.body as string
     try {
         const coach = Object.assign(new RegisterCoachDto, JSON.parse(datas))
-
+        await validate(coach).then((result) => {
+            const error = result[0]
+            if(error !== undefined && error.constraints){
+                throw new UserError(401, 'UserApiErrors', `${error.property}: ${error.value} is not valide`, coach)
+            }
+        })
         return {
             statusCode:200,
             body: coach
         }
-    }catch (e) {
+    }catch (e : UserError|any) {
         return {
-            statusCode: 401,
-            body: e
+            statusCode: e.status,
+            datas: e
         }
     }
 
