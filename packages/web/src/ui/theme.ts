92e30fa406f67cdb1db8db1ee6b075dfbf51f15a
stephane <stephane@khedhiri.com>
2023-06-20 16:51:19 +0200

    add theme

---
+++ packages/web/src/ui/theme.ts
@@ -0,0 +1,23 @@
+export enum ThemeColors {
+    PRIMARY = 'primary',
+    SECONDARY = 'secondary',
+    TERTIARY = 'tertiary',
+    YELLOW = 'yellow',
+    GREEN = 'green',
+    RED = 'red',
+    BLUE = 'blue',
+    PURPLE = 'purple',
+}
+
+export enum ThemeSizes {
+    SM = 'sm',
+    MD = 'md',
+    LG = 'lg',
+    XL = 'xl',
+    '2XL' = '2xl',
+}
+
+export enum ButtonTypes {
+    NOT_PLAIN = 'not-plain',
+    OUTLINE = 'outline',
+}
