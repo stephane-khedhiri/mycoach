476e123af7e7efc968b8c24e02ad205941b3a62b
stephane <stephane@khedhiri.com>
2023-07-10 19:32:14 +0200

    add user repostory

--- packages/api/src/coach/list.ts
+++ packages/api/src/coach/list.ts
@@ -2,13 +2,16 @@ import "reflect-metadata"
 import {APIGatewayProxyHandlerV2WithLambdaAuthorizer} from "aws-lambda";
 import {UserRepository} from "@mycoach/core/repositories/user.repositories";
 import {UserPayloadWithJwt} from "@mycoach/core/types";
-
+import {connection} from "@mycoach/core/connection";
+const datasource = connection()
+const userRepository = new UserRepository(datasource)
 export const handler: APIGatewayProxyHandlerV2WithLambdaAuthorizer<UserPayloadWithJwt> = async (event) => {
+    await datasource.initialize()
     try {
-        const coachs = await new UserRepository().all()
+        const users = await userRepository.all()
         return {
             statusCode: 200,
-            body: JSON.stringify(coachs)
+            body: JSON.stringify(users)
         }
     }catch (e: any) {
         return {
@@ -17,6 +20,10 @@ export const handler: APIGatewayProxyHandlerV2WithLambdaAuthorizer<UserPayloadWi
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(e.message)
+        }
+    }finally {
+        if(datasource.isInitialized){
+            await datasource.destroy()
         }
     }
 
