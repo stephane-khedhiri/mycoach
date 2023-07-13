2e3c65ce38aaad169541f0f82d85b91f4d047908
stephane <stephane@khedhiri.com>
2023-07-13 14:36:43 +0200

    fix key

--- packages/core/src/util/jwt.ts
+++ packages/core/src/util/jwt.ts
@@ -2,18 +2,29 @@ import {JwtPayload, sign, verify} from "jsonwebtoken"
 import {UnAuthorized} from "../error/errors";
 
 
-
-export const generatedToken = (payload:object, secretsPrivateBase64: string ) => {
+export const generatedToken = (payload: object, secretsPrivateBase64: string) => {
     const secretsPrivate = Buffer.from(secretsPrivateBase64, 'base64').toString('utf-8')
-    const key= Buffer.from(secretsPrivate, 'base64')
-    return sign({...payload}, `${key}`)
+    return sign(
+        {...payload},
+        secretsPrivate,
+        {
+            algorithm: "RS256",
+            expiresIn: 15 * 60000, // 15m
+        })
 }
-export const verifyTokenOrThrow = (token: string, secretsPublicBase64: string)  => {
+export const verifyTokenOrThrow = (token: string, secretsPublicBase64: string): Promise<JwtPayload> => {
     const secretsPublic = Buffer.from(secretsPublicBase64, 'base64').toString('utf-8')
-    const key= Buffer.from(secretsPublic, 'base64')
-    const payload = verify(token, key) as JwtPayload
-    if (!payload) {
-        throw new UnAuthorized()
-    }
-    return payload;
+    return new Promise((resolve) => {
+        verify(token, secretsPublic, {algorithms: ["RS256"]}, (error, decoded) => {
+            if (error) {
+                throw error
+            }
+            if (!decoded){
+                throw new UnAuthorized()
+            }
+            resolve(decoded as JwtPayload)
+        })
+    })
+
+
 }
