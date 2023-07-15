013c81a0361f10c26b045069ef26ee4b2b61f0e1
stephane <stephane@khedhiri.com>
2023-07-15 15:28:00 +0200

    fix key

--- packages/core/src/util/jwt.ts
+++ packages/core/src/util/jwt.ts
@@ -2,20 +2,19 @@ import {JwtPayload, sign, verify} from "jsonwebtoken"
 import {UnAuthorized} from "../error/errors";
 
 
-export const generatedToken = (payload: object, secretsPrivateBase64: string) => {
-    const secretsPrivate = Buffer.from(secretsPrivateBase64, 'base64').toString('utf-8')
+export const generatedToken = (payload: object, privateKey: Buffer |string) => {
+
     return sign(
         {...payload},
-        secretsPrivate,
+        typeof privateKey === 'string'? privateKey: privateKey.toString('utf-8'),
         {
             algorithm: "RS256",
-            expiresIn: 15 * 60000, // 15m
+            expiresIn: '1h', // 15m
         })
 }
-export const verifyTokenOrThrow = (token: string, secretsPublicBase64: string): Promise<JwtPayload> => {
-    const secretsPublic = Buffer.from(secretsPublicBase64, 'base64').toString('utf-8')
+export const verifyTokenOrThrow = (token: string, publicKey: Buffer | string): Promise<JwtPayload> => {
     return new Promise((resolve) => {
-        verify(token, secretsPublic, {algorithms: ["RS256"]}, (error, decoded) => {
+        verify(token, typeof publicKey === 'string'? publicKey : publicKey.toString('utf-8'), {algorithms: ["RS256"]}, (error, decoded) => {
             if (error) {
                 throw error
             }
