314e74422b2434684645948f983ce5443c2d450e
stephane <stephane@khedhiri.com>
2023-07-17 23:13:48 +0200

    add offer projection

--- packages/core/src/projection/coach/userProjection.ts
+++ packages/core/src/projection/coach/userProjection.ts
@@ -1,4 +1,5 @@
-import {Expose} from 'class-transformer'
+import {Expose, Type} from 'class-transformer'
+import {OfferProjection} from "../offer.projection";
 
 export class UserProjection {
     @Expose()
@@ -6,4 +7,7 @@ export class UserProjection {
     @Expose()
     email: string
     password: string
+    @Expose()
+    @Type(() => OfferProjection)
+    offers: OfferProjection[]
 }
