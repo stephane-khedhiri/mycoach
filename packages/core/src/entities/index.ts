ebf76216086e774b71d05a0d113bf0aa519da274
stephane <stephane@khedhiri.com>
2023-07-17 21:41:47 +0200

    fix index

--- packages/core/src/entities/index.ts
+++ packages/core/src/entities/index.ts
@@ -1,3 +1,8 @@
-import {UserEntity} from "./user.entity";
+import {UserEntity} from "./users";
+import {OfferEntity} from "./offer.entity";
+import {CoachEntity} from "./coach.entity";
 
-export const entities = [UserEntity];
+export type UserEntityType = typeof UserEntity.prototype
+export type OfferEntityType = typeof OfferEntity.prototype
+export type CoachEntityType = typeof CoachEntity.prototype
+export const entities = [CoachEntity, OfferEntity];
