1f330721b83a0f69bfa308291bbec198d18b5487
stephane <stephane@khedhiri.com>
2023-07-17 14:33:37 +0200

    fix update

--- packages/core/src/repositories/user.repositories.ts
+++ packages/core/src/repositories/user.repositories.ts
@@ -8,9 +8,15 @@ export class UserRepository {
     constructor(private readonly db: DataSource ) {}
 
 
-    public async users(selects: SelectQueryBuilder<UserEntity>['selects']) {
+    public async users(selects?: SelectQueryBuilder<UserEntity>['selects']) {
+
         try {
             await this.db.initialize()
+            if(!selects){
+                return  this.db.getRepository(UserEntity)
+                    .createQueryBuilder('user')
+                    .getMany()
+            }
             return  this.db.getRepository(UserEntity)
                 .createQueryBuilder('user')
                 .select(selects.map(select => `user.${select}`))
@@ -80,7 +86,8 @@ export class UserRepository {
     public async update(user: UserEntity){
         try{
             await this.db.initialize()
-            return this.db.getRepository(UserEntity).save(user)
+            const userEntity = this.db.getRepository(UserEntity).create(user)
+            return this.db.getRepository(UserEntity).save(userEntity)
         }catch (err){
             throw err
         }finally {
