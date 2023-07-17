d83ff2236983ac3615dd58f9ba893241e23ae7fe
stephane <stephane@khedhiri.com>
2023-07-17 14:26:19 +0200

    add user admin default

--- packages/core/src/projection/token.projection.ts
+++
@@ -1,8 +0,0 @@
-import {Expose} from "class-transformer";
-import {UserProjection} from "./coach/userProjection";
-
-export class TokenProjection {
-    @Expose({name: 'AccessToken'})
-    accessToken: string
-    data?: UserProjection[]
-}
