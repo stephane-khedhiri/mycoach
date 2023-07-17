98e76802678de6563ed1dacd3ab62badbafcd2b3
stephane <stephane@khedhiri.com>
2023-07-17 21:44:50 +0200

    fix dto

--- packages/core/src/dto/coach/create.coach.dto.ts
+++ packages/core/src/dto/coach/create.coach.dto.ts
@@ -1,4 +1,4 @@
-import {IsEmail, IsString, IsNotEmpty, ValidateNested} from 'class-validator'
+import {IsEmail, IsString, IsNotEmpty} from 'class-validator'
 import {IsUserUnique} from "../../decorator";
 
 
@@ -6,14 +6,22 @@ import {IsUserUnique} from "../../decorator";
 
 
 
-export class CreateUserDto {
+export class CreateCoachDto {
     @IsNotEmpty({message: ({property}) =>  `${property} is required` })
     @IsString()
 
-    readonly password: string;
+    readonly password: string
 
     @IsNotEmpty({message: ({property}) =>  `${property} is required` })
     @IsEmail()
     @IsUserUnique()
-    readonly email: string;
+    readonly email: string
+
+    @IsNotEmpty({message: ({property}) =>  `${property} is required` })
+    @IsString()
+    readonly firstName: string
+
+    @IsNotEmpty({message: ({property}) =>  `${property} is required` })
+    @IsString()
+    readonly lastName: string
 }
