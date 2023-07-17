f6fca56c9b4d16231a9d06034c826338a2e5d22d
stephane <stephane@khedhiri.com>
2023-07-17 12:18:37 +0200

    create method existbyEmail

--- packages/core/src/repositories/user.repositories.ts
+++ packages/core/src/repositories/user.repositories.ts
@@ -1,7 +1,7 @@
 import type {DataSource} from "typeorm"
-import * as uuid from 'uuid'
 import {UserEntity} from "../entities/user.entity";
 import {CreateUserDto} from "../dto/coach/create.user.dto";
+import {UpdateCoachDto} from "../dto/coach/update.coach.dto";
 
 
 
@@ -22,11 +22,12 @@ export class UserRepository {
         }
     }
 
-    public async userById(id: string) {
+    public async userById(id: string, selects: string[]| string) {
         try {
             await this.db.initialize()
             return this.db.getRepository(UserEntity)
                 .createQueryBuilder("user")
+                .select([...selects])
                 .where("user.id = CAST(:id AS UUID)", { id })
                 .getOne()
         }catch (err) {
@@ -49,12 +50,36 @@ export class UserRepository {
             }
         }
     }
+
+    public async existByEmail(email: string) {
+        try {
+            await this.db.initialize()
+            return await this.db.getRepository(UserEntity).createQueryBuilder('c').where('c.email = :email', {email}).getCount()
+        }catch (err) {}
+        finally {
+            if(this.db.isInitialized){
+                await this.db.destroy()
+            }
+        }
+    }
     public async create(user: CreateUserDto){
         try {
             await this.db.initialize()
             const userEntity = this.db.getRepository(UserEntity).create(user)
             return  this.db.getRepository(UserEntity).save(userEntity)
         }catch (err) {
+            throw err
+        }finally {
+            if(this.db.isInitialized){
+                await this.db.destroy()
+            }
+        }
+    }
+    public async update(user: UserEntity){
+        try{
+            await this.db.initialize()
+            return this.db.getRepository(UserEntity).save(user)
+        }catch (err){
             throw err
         }finally {
             if(this.db.isInitialized){
