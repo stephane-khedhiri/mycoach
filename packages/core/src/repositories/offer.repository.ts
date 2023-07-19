967d7a762c230ddd4b10d9eeedead0e4236299d8
stephane <stephane@khedhiri.com>
2023-07-19 14:41:46 +0200

    fix create offer with coach

--- packages/core/src/repositories/offer.repository.ts
+++ packages/core/src/repositories/offer.repository.ts
@@ -1,5 +1,6 @@
 import {DataSource} from "typeorm";
 import {OfferEntity} from "../entities/offer.entity";
+import {CoachEntity} from "../entities/coach.entity"
 
 export class OfferRepository {
     constructor(private readonly db: DataSource) {}
@@ -31,10 +32,16 @@ export class OfferRepository {
             }
         }
     }
-    public async createByCoach(coachId: string, offer: Partial<OfferEntity>){
+    public async createByCoach(coachId: string, data: Partial<OfferEntity>){
         try{
             await this.db.initialize()
-            return this.db.getRepository(OfferEntity).save(Object.assign(offer, coachId))
+            const coach  = await this.db.getRepository(CoachEntity).findOne({where:{id: coachId}})
+            if(!coach){
+                throw new Error()
+            }
+            const offer = this.db.getRepository(OfferEntity).create(Object.assign(OfferEntity, {coach, ...data}))
+
+            return this.db.getRepository(OfferEntity).save(offer)
         }catch (e) {
         }finally {
             if(this.db.isInitialized){
