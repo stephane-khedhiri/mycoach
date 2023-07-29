999437a2660dcb9b3a3f37e40abc7b21d29bb102
stephane <stephane@khedhiri.com>
2023-07-30 00:00:51 +0200

    create commande

--- packages/core/src/entities/index.ts
+++ packages/core/src/entities/index.ts
@@ -2,7 +2,8 @@ import {UserEntity} from "./users";
 import {OfferEntity} from "./offer.entity";
 import {CoachEntity} from "./coach.entity";
 
+import {SportsmenEntity} from "./sportsmen.entity";
+
 export type UserEntityType = typeof UserEntity.prototype
-export type OfferEntityType = typeof OfferEntity.prototype
-export type CoachEntityType = typeof CoachEntity.prototype
-export const entities = [CoachEntity, OfferEntity];
+
+export const entities = [CoachEntity, OfferEntity, SportsmenEntity];
