2d51e9a3fdb638f60064985acc3c322d99f1425a
stephane <stephane@khedhiri.com>
2023-07-30 00:26:40 +0200

    add relation sportsman -> commandes

--- packages/core/src/entities/sportsmen.entity.ts
+++ packages/core/src/entities/sportsmen.entity.ts
@@ -1,7 +1,8 @@
 import {UserEntity} from "./users";
 import {Entity, OneToMany} from "typeorm";
-
+import {CommandeEntity} from "./commande.entity";
 @Entity('sportsMen')
 export class SportsmenEntity extends UserEntity{
-
+    @OneToMany(() => CommandeEntity, (commandes) => commandes.sportsman)
+    commandes: CommandeEntity[]
 }
