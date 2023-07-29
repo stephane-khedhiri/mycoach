996fecad9dcdd38744ecbcb1dd9d2072feffaeb0
stephane <stephane@khedhiri.com>
2023-07-29 18:38:17 +0200

    add column apipaypal

--- packages/core/src/entities/coach.entity.ts
+++ packages/core/src/entities/coach.entity.ts
@@ -5,6 +5,8 @@ import {OfferEntity} from "./offer.entity";
 
 @Entity('coachs')
 export class CoachEntity extends UserEntity {
+    @Column()
+    apiPaypal: string
     @OneToMany(() => OfferEntity, (offers) => offers.coach)
     offers: OfferEntity[]
 }
