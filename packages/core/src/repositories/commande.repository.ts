48382a2447052bef01601620cd100b90b2d3ed42
stephane <stephane@khedhiri.com>
2023-07-31 12:00:56 +0200

    add method findByCoachWithId

--- packages/core/src/repositories/commande.repository.ts
+++ packages/core/src/repositories/commande.repository.ts
@@ -1,5 +1,24 @@
 import {DataSource} from "typeorm";
+import {CommandeEntity} from "../entities/commande.entity";
 
 export class CommandeRepository {
     constructor(private readonly db: DataSource) {}
+
+    public async findByCoachWithId (id: string){
+        try{
+
+            await this.db.initialize()
+            return this.db.getRepository(CommandeEntity)
+                .createQueryBuilder('c')
+                .innerJoin("c.sportsman", "sportsmen")
+                .where('c.coachId = :id', {id})
+                .getMany()
+        }catch (err) {
+            throw err
+        }finally {
+            if(this.db.isInitialized){
+                await this.db.destroy()
+            }
+        }
+    }
 }
