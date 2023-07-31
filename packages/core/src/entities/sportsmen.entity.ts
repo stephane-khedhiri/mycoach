8e65185b199202ebd856a63c762293205269e5bb
stephane <stephane@khedhiri.com>
2023-07-31 11:58:05 +0200

    remove column

--- packages/core/src/entities/sportsmen.entity.ts
+++ packages/core/src/entities/sportsmen.entity.ts
@@ -1,8 +1,10 @@
-import {Column, Entity, OneToMany} from "typeorm";
+import {BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn} from "typeorm";
 import {CommandeEntity} from "./commande.entity";
 
 @Entity('sportsmen')
-export class SportsmenEntity {
+export class SportsmenEntity extends BaseEntity{
+    @PrimaryGeneratedColumn('uuid')
+    id: string
     @Column()
     email: string
     @OneToMany(() => CommandeEntity, (commandes) => commandes.sportsman)
