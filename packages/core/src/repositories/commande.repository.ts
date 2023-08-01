a346180a8d748fe8cdf95f0b496d024aa939a627
stephane <stephane@khedhiri.com>
2023-08-01 17:28:42 +0200

    add method create

--- packages/core/src/repositories/commande.repository.ts
+++ packages/core/src/repositories/commande.repository.ts
@@ -1,5 +1,8 @@
 import {DataSource} from "typeorm";
 import {CommandeEntity} from "../entities/commande.entity";
+import {SportsmenEntity} from "../entities/sportsmen.entity";
+import {CoachEntity} from "../entities/coach.entity";
+import {OfferEntity} from "../entities/offer.entity";
 
 export class CommandeRepository {
     constructor(private readonly db: DataSource) {}
@@ -15,6 +18,35 @@ export class CommandeRepository {
                 .getMany()
         }catch (err) {
             throw err
+        }finally {
+            if(this.db.isInitialized){
+                await this.db.destroy()
+            }
+        }
+    }
+    public async create (commande: { paymentId: string, sportsman: SportsmenEntity, offer: OfferEntity, coach:CoachEntity }) {
+        try{
+            console.log(commande)
+            await this.db.initialize()
+            const commandeEntity = this.db.getRepository(CommandeEntity).create(Object.assign(CommandeEntity, commande))
+            return this.db.getRepository(CommandeEntity).save(commandeEntity)
+        }catch (e) {
+            throw e
+        }finally {
+            if(this.db.isInitialized){
+                await this.db.destroy()
+            }
+        }
+    }
+    public async delete(id: string) {
+        try{
+            await this.db.initialize()
+            return this.db.getRepository(CommandeEntity).createQueryBuilder('c')
+                .delete()
+                .where("c.id = :id", {id})
+                .execute()
+        }catch (e) {
+            throw e
         }finally {
             if(this.db.isInitialized){
                 await this.db.destroy()
