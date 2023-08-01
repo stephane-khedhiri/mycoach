0b9800e994e38dd7a60d09b7694ed688bbee71e4
stephane <stephane@khedhiri.com>
2023-08-01 16:59:34 +0200

    fix decorator

--- packages/core/src/decorator/isUserUnique.ts
+++ packages/core/src/decorator/isUserUnique.ts
@@ -4,23 +4,25 @@ import {
     ValidationArguments,
     ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface,
 } from "class-validator";
-import {CoachRepository} from "../repositories/";
 import {DataSource} from "typeorm";
 import {databaseConfig} from "../config/database.conf";
+import {UserRepository} from "../repositories/user.repository";
 
-const coachRepository = new CoachRepository(new DataSource(databaseConfig))
+const userRepository = new UserRepository(new DataSource(databaseConfig))
 
 @ValidatorConstraint()
 export class UniqueOnDatabaseExistConstraint implements ValidatorConstraintInterface {
-    validate(value: any, args: ValidationArguments) {
-        return coachRepository.existByEmail(value).then((exist) => !exist)
+    validate(value: any, args: ValidationArguments & {object: Object & {entity: Function}} ) {
+
+        return userRepository.existByEmail(args.object.entity ,value).then((exist) => !exist)
     }
     defaultMessage(args: ValidationArguments) {
         return `${args.property} already exists`; // Message d'erreur personnalisé
     }
 }
-export function IsUserUnique(validationOptions?: ValidationOptions) {
+export function IsUserUnique(entity: Function, validationOptions?: ValidationOptions & {entity?: Function}) {
     return function (object: Object, propertyName: string) {
+        object = {...object, entity}
         registerDecorator({
             name: 'isUserAlreadyExist',
             target: object.constructor,
