5905cf8f9fc57384518f469aa6dac7b8c641c61e
stephane <stephane@khedhiri.com>
2023-08-02 11:46:28 +0200

    fix path

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import {AppRoutes} from '@routing/index'
+import '@ui/assets/css/style.css'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AppRoutes/>
   </React.StrictMode>,
 )
