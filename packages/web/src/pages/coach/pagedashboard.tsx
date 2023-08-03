e946e2afa02a250f93f8ef18e088a926bfe68328
stephane <stephane@khedhiri.com>
2023-08-03 22:51:10 +0200

    create dashbord

---
+++ packages/web/src/pages/coach/pagedashboard.tsx
@@ -0,0 +1,14 @@
+import {FC, useEffect} from "react";
+
+
+export const PageDashboard: FC = () => {
+    useEffect(()=> {
+        window.document.title = "Dashboard"
+    })
+
+    return (
+
+            'qsdfqse'
+
+    )
+}
