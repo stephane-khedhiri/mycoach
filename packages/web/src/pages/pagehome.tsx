21f9ef9e8a4a86d88058624958997089bcefb0af
stephane <stephane@khedhiri.com>
2023-08-03 23:03:11 +0200

    remove layout

--- packages/web/src/pages/pagehome.tsx
+++ packages/web/src/pages/pagehome.tsx
@@ -1,5 +1,4 @@
 import {FunctionComponent} from 'react'
-import { Layout } from '../ui/layout/index'
 import {useQuery} from "react-query";
 import OffersService from "../service/offers.service";
 import {CradList} from "../ui/layout/component/card";
@@ -7,28 +6,26 @@ import {CradOffer} from "../ui/layout/component/card/components/crad.offer";
 
 
 export const PageHome: FunctionComponent = () => {
-    const { data, isLoading } = useQuery('offers', async ()  => {
+    const {data, isLoading} = useQuery('offers', async () => {
         const {data} = await OffersService.gets()
         return data
     })
-    if(isLoading) return <div>Loading...</div>;
+    if (isLoading) return <div>Loading...</div>;
     return (
-        <Layout>
-
+        <>
             {isLoading ?
                 <div>Loading..</div>
                 :
-                Array.isArray(data)?
+                Array.isArray(data) ?
                     <CradList>
                         {data.map((item, index) =>
-                            (<CradOffer key={index} id={item.id} contents={item.content} name={item.name} price={item.price}/>)
+                            (<CradOffer key={index} id={item.id} contents={item.content} name={item.name}
+                                        price={item.price}/>)
                         )}
                     </CradList>
                     :
                     <div> aucune offer pour le moment</div>
             }
-
-
-        </Layout>
+        </>
     )
 }
