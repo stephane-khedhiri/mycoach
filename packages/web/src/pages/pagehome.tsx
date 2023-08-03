922deedbe53e6a200347a1be0deecdd464a2aa6a
stephane <stephane@khedhiri.com>
2023-08-03 22:56:35 +0200

    add cardList

--- packages/web/src/pages/pagehome.tsx
+++ packages/web/src/pages/pagehome.tsx
@@ -3,14 +3,14 @@ import { Layout } from '../ui/layout/index'
 import {useQuery} from "react-query";
 import OffersService from "../service/offers.service";
 import {CradList} from "../ui/layout/component/card";
+import {CradOffer} from "../ui/layout/component/card/components/crad.offer";
 
 
 export const PageHome: FunctionComponent = () => {
     const { data, isLoading } = useQuery('offers', async ()  => {
         const {data} = await OffersService.gets()
-        return Object.values(data)[0]
+        return data
     })
-
     if(isLoading) return <div>Loading...</div>;
     return (
         <Layout>
@@ -19,7 +19,11 @@ export const PageHome: FunctionComponent = () => {
                 <div>Loading..</div>
                 :
                 Array.isArray(data)?
-                    <CradList contents={data}/>
+                    <CradList>
+                        {data.map((item, index) =>
+                            (<CradOffer key={index} id={item.id} contents={item.content} name={item.name} price={item.price}/>)
+                        )}
+                    </CradList>
                     :
                     <div> aucune offer pour le moment</div>
             }
